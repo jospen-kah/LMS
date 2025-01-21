a143c4da74a4655dc6e08543cfe8b6d423ae7d09
jospen-kah <ngumkahjospen@gmail.com>
2025-01-21 19:00:03 +0100

    benefits

--- client/client_app/src/pages/Home.jsx
+++ client/client_app/src/pages/Home.jsx
@@ -1,14 +1,23 @@
-import {Zap} from 'lucide-react'
+import { Zap } from 'lucide-react'
 
 
 // 
 const Home = () => {
+
+    const texts = [
+        ["I am the true son", "Hello", "food"],
+        ["I am the true son", "Hello", "food"],
+        ["I am the true son", "Hello", "food"],
+        ["I am the true son", "Hello", "food"],
+        ["I am the true son", "Hello", "food"],
+        ["I am the true son", "Hello", "food"]
+    ]
     return (
         <div className="home">
             <div className=" content-1">
                 <div className="quotes">
                     <div className="quote-1">
-                    <Zap fill size={20} color="#000"  className="zap"/>
+                        <Zap fill size={20} color="#000" className="zap" />
                         <h2><span>Unlock</span> Your Creative Potential</h2>
                     </div>
                     <div className="quote-2">
@@ -22,10 +31,38 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="bg-image">
-                    <img src="https://media.istockphoto.com/id/1059510610/vector/it-communication-e-learning-internet-network-as-knowledge-base.jpg?s=612x612&w=0&k=20&c=QEyHx6JnZleLmW9lYgpzvLv765rizr__5zwwKylo300=" alt="img"/>
+                    <img src="https://media.istockphoto.com/id/1059510610/vector/it-communication-e-learning-internet-network-as-knowledge-base.jpg?s=612x612&w=0&k=20&c=QEyHx6JnZleLmW9lYgpzvLv765rizr__5zwwKylo300=" alt="img" />
                 </div>
             </div>
+            <div className="content-2">
+                <div className='benefits'>
+                    <h3>Benefits</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur. Tempus tincidunt
+                        etiam eget elit id imperdiet et. Cras eu sit dignissim lorem
+                        nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.</p>
+                </div>
+                <div className="benefits-list">
+                    {Array.from({ length: texts.length }).map((_, index) => (
+                        <div key={index} className="item">
+                            <div className="avatar"> {index + 1}</div>
+                            <div className="text-list">
+                                {texts[index].map((text, textIndex) => (
+                                    <p key={textIndex} className="text-item">{text}</p>
+                                ))}
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
+            </div>
+
         </div>
     )
 }
 export default Home;
+
+
+
+
+
+
